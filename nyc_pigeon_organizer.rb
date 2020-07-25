99b4c5915d940ee1ae450ff7009058b3ec3a70e4
Chandler Hanson <chan.hanson5@gmail.com>
2020-07-25 01:08:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
-      name.each do |name|
+      names.each do |name|
         if !final_array[name]
           final_array[name] = {}
         end
@@ -14,5 +14,5 @@ def nyc_pigeon_organizer(data)
       final_array[name][key].push(inner_key.to_s)
     end
   end
-  final_results
+  #final_results
 end
